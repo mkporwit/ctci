f139563f2af8c1537da202c480c9032ec1332f0c
Marcin Krzysztof Porwit <mkporwit@porwit.net>
2016-08-04 10:49:54 -0700

    Implemented removeDuplicates, getKthToLast, deleteFromMiddle, and partition Fixed append behavior to allow for appending a list to a list

--- LinkedLists/src/main/java/net/porwit/ctci/LinkedLists/Node.java
+++ LinkedLists/src/main/java/net/porwit/ctci/LinkedLists/Node.java
@@ -1,6 +1,6 @@
 package net.porwit.ctci.LinkedLists;
 
-class Node<E> {
+class Node<E extends Comparable<E>> {
     Node<E> next = null;
     E data;
 
@@ -10,10 +10,11 @@ class Node<E> {
 
     public Node<E> append(Node<E> node) {
         Node<E> runner = this;
-        while(this.next != null) {
+        while(runner.next != null) {
             runner = runner.next;
         }
-        node.next = runner.next;
+        // Don't need this since each new node is null-terminated by default, and this way it can append a node or a list to an existing list
+        //node.next = runner.next;
         runner.next = node;
 
         return this;
@@ -30,7 +31,91 @@ class Node<E> {
         return false;
     }
     
-    public Node<E> removeDuplicates(Node<E> list) {
-        return list;
+    public Node<E> removeDuplicates() {
+    	Node<E> runner = this;
+    	Node<E> newList = new Node<E>(this.data);
+    	while(runner != null) {
+    		if(!newList.contains(runner.data)) {
+    			newList = newList.append(new Node<E>(runner.data));
+    		}
+    		runner = runner.next;
+    	}
+    	return newList;
+    }
+    
+    /**
+     * Returns the kth from end element of the list, zero indexed. So, if k is zero, it will return the last element
+     * 
+     * @param k -- the offset of the node from the end, zero-indexed
+     * @return -- a reference to the node
+     * @throws -- IllegalArgumentException if k < 0
+     * @throws -- IndexOutOfBoundsException if k is greater than the number of nodes in the list
+     */
+    public Node<E> getKthToLast(int k) throws IllegalArgumentException {
+    	if(k < 0) {
+    		throw new IllegalArgumentException("k must be a positive value");
+    	}
+    	Node<E> cur = this;
+    	Node<E> runner = this;
+    	for(int i = 0; i < k; i++) {
+    		if(runner.next != null) {
+    			runner = runner.next;
+    		} else {
+    			throw new IndexOutOfBoundsException("k was [" + k + "] but only found [" + i + 1 + "] nodex");
+    		}
+    	}
+    	while(runner.next != null) {
+    		cur = cur.next;
+    		runner = runner.next;
+    	}
+    	
+    	return cur;
+    }
+    
+    public void deleteFromMiddle(Node<E> node) {
+    	if(node == null) {
+    		return;
+    	} else {
+    		Node<E> runner = node.next;
+    		while(runner != null) {
+    			node.data = runner.data;
+    			node = runner;
+    			runner = runner.next;
+    		}
+    	}
+    }
+    
+    public Node<E> partition(E data) {
+    	if(data == null) {
+    		return this;
+    	}
+    	if(!this.contains(data)) {
+    		return this;
+    	}
+    	Node<E> leftPart = null, rightPart = null, middlePart = null, runner = this;
+    	while(runner != null) {
+    		Node<E> n = new Node<E>(runner.data);
+    		if(runner.data.compareTo(data) == 0) {
+    			if(middlePart == null) {
+    				middlePart = n;
+    			} else {
+    				middlePart = middlePart.append(n);
+    			}
+    		} else if (runner.data.compareTo(data) < 0) {
+    			if(leftPart == null) {
+    				leftPart = n;
+    			} else {
+    				leftPart = leftPart.append(n);
+    			}
+    		} else if (runner.data.compareTo(data) > 0) {
+    			if(rightPart == null) {
+    				rightPart = n;
+    			} else {
+    				rightPart = rightPart.append(n);
+    			}
+    		}
+    		runner = runner.next;
+    	}
+    	return leftPart.append(middlePart.append(rightPart));
     }
 }
--- LinkedLists/src/test/java/net/porwit/ctci/LinkedLists/NodeTest.java
+++ LinkedLists/src/test/java/net/porwit/ctci/LinkedLists/NodeTest.java
@@ -21,7 +21,142 @@ public class NodeTest {
     @Test
     public void appendTest() {
         Node<String> list = new Node<String>("aa");
-        list = list.append(new Node<String>("aa"));
         assertNotNull(list);
+        list = list.append(new Node<String>("bb"));
+        assertTrue("append contains success", list.contains("bb"));
+        list = list.append(new Node<String>("cc"));
+        assertTrue("append success again", list.contains("cc"));
+        // Verify order
+        Node<String> runner = list;
+        assertNotNull(runner);
+        assertEquals("first node is aa", "aa", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("second node is bb", "bb", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("third node is cc", "cc", runner.data);
+        runner = runner.next;
+        assertEquals("end of the line", null, runner);
+    }
+    
+    @Test
+    public void removeDuplicatesTest() {
+    	Node<String> list = new Node<String>("aa");
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("aa"));
+    	assertTrue("contains duplicate", list.next.contains("aa"));
+    	list = list.removeDuplicates();
+    	assertFalse("no longer contains duplicate", list.next.contains("aa"));
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("dd"));
+    	assertTrue("contains duplicate bb", list.next.next.contains("bb"));
+    	assertTrue("contains duplicate cc", list.next.next.next.contains("cc"));
+    	list = list.removeDuplicates();
+    	assertFalse("no longer contains duplicate bb", list.next.next.contains("bb"));
+    	assertFalse("no longer contains duplicate cc", list.next.next.next.contains("cc"));
+    	// Verify order
+        Node<String> runner = list;
+        assertNotNull(runner);
+        assertEquals("first node is aa", "aa", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("second node is bb", "bb", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("third node is cc", "cc", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("fourth node is dd", "dd", runner.data);
+        runner = runner.next;
+        assertEquals("end of the line", null, runner);
+    }
+    
+    @Test
+    public void findKthToLastTest() {
+    	Node<String> list = new Node<String>("aa");
+       	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("dd"));
+    	list = list.append(new Node<String>("ee"));    	
+    	list = list.append(new Node<String>("ff"));    	
+    	list = list.append(new Node<String>("gg"));
+    	Node<String> kth = list.getKthToLast(3);
+    	assertEquals("4th from last is dd", "dd", kth.data);
+    	kth = list.getKthToLast(0);
+    	assertEquals("0th from last is gg (last node", "gg", kth.data);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void findKthToLastNegativeArgTest() {
+    	Node<String> list = new Node<String>("aa");
+       	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("dd"));
+    	list = list.append(new Node<String>("ee"));    	
+    	list = list.append(new Node<String>("ff"));    	
+    	list = list.append(new Node<String>("gg"));
+    	Node<String> kth = list.getKthToLast(-1);
+    }
+    
+    @Test(expected=IndexOutOfBoundsException.class) 
+    public void findKthToLastOutOfBoundsArgTest() {
+    	Node<String> list = new Node<String>("aa");
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("dd"));
+    	list = list.append(new Node<String>("ee"));    	
+    	list = list.append(new Node<String>("ff"));    	
+    	list = list.append(new Node<String>("gg"));
+    	Node<String> kth = list.getKthToLast(10);
+    }
+    
+    @Test
+    public void deleteFromMiddleTest() {
+    	Node<String> list = new Node<String>("aa");
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list.deleteFromMiddle(list.next);
+    	assertTrue("still contains aa", list.contains("aa"));
+    	assertTrue("still contains cc", list.contains("cc"));
+    	assertFalse("does not contain bb", list.contains("bb"));
+    }
+    
+    @Test
+    public void partitionTest() {
+    	Node<String> list = new Node<String>("aa");
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("cc"));
+    	list = list.append(new Node<String>("aa"));
+    	list = list.append(new Node<String>("dd"));
+    	list = list.append(new Node<String>("bb"));
+    	list = list.append(new Node<String>("ee"));
+    	list = list.partition("cc");
+    	// Verify order
+        Node<String> runner = list;
+        assertNotNull(runner);
+        assertEquals("first node is aa", "aa", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("second node is bb", "bb", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("third node is aa", "aa", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("fourth node is bb", "bb", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("fifth node is cc", "cc", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("sixth node is dd", "dd", runner.data);
+        runner = runner.next;
+        assertNotNull(runner);
+        assertEquals("seventh node is ee", "ee", runner.data);
+        runner = runner.next;
+        assertEquals("end of the line", null, runner);
     }
 }
