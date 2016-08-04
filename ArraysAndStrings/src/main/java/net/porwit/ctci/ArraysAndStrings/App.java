406936a608691609c9bc3587ddfc60d7ab9206d2
Marcin Krzysztof Porwit <mkporwit@porwit.net>
2016-08-04 15:39:25 -0700

    Initial ArraysAndStrings project directory

---
+++ ArraysAndStrings/src/main/java/net/porwit/ctci/ArraysAndStrings/App.java
@@ -0,0 +1,13 @@
+package net.porwit.ctci.ArraysAndStrings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
